0e3e7ce0183869ad66f21b50e9eeaa6104c9eff0
juandesousa <juan.cdsb@gmail.com>
2022-08-16 21:10:35 +0200

    new feature

--- scripts/app.js
+++ scripts/app.js
@@ -4,26 +4,46 @@ console.log("Script conectado correctamente")
 
 const API_URL = "https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects"
 
+const random = (a, b) => {
+    return 0.5 - Math.random();
+}
+
+const createCard = (element) => {
+    return `
+    <div class="card-box card-box-hover">
+    <img src="${element.image}" alt="img - ${element.name}">
+    <h3>${element.name}</h3>
+    <p>${element.description}</p>
+    <a href="./pages/${element.name}.html">Learn more</a>
+    </div>
+`
+}
+
+const card = document.getElementById("cards-projects");
 
 
-const fectAPI = async ()=>{
+const fectAPI = async () => {
     try {
 
         const data = await fetch(API_URL)
         return await data.json()
-        
+
     } catch (error) {
         return []
     }
 }
 
 
-const apppGlobal = async() =>{
+const appGlobal = async () => {
+    card.innerHTML = "";
+    const result = await fectAPI();
+    const randomCard = result.sort(random);
 
-    const result = await fectAPI()
-    console.log(result)
+    randomCard.slice(-3).forEach(element => {
+        card.innerHTML += createCard(element)
+    });
 
 }
 
-apppGlobal()
+appGlobal()
 
