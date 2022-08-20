6e63e2ce2a06a3c9d6099e41d989116b8f906d8b
JReuman1 <99495156+JReuman1@users.noreply.github.com>
2022-08-20 12:34:54 +0200

    prueba

--- scripts/app.js
+++ scripts/app.js
@@ -237,5 +237,20 @@ const appGlobal = async () => {
 
 }
 
+/* Hamburguer */
+const hamburguer = document.querySelector(".hamburguer");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburguer.addEventListener("click", () => {
+    hamburguer.classList.toggle("active");
+    navMenu.classList.toggle("active");})
+
+document.querySelectorAll(".nav-link").forEach(
+    n=> n.addEventListener("click", () => {
+        hamburguer.classList.remove("active");
+        navMenu.classList.remove("active");
+    }
+))
+
 appGlobal()
 
